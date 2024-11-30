e5b67cce14aef65a8469e16d62a0a3015e32c2df
DharDhar <dhardhar2006@gmail.com>
2024-11-30 15:27:41 +0530

    four-directional walk cycles for red

--- include/red_ow.h
+++ include/red_ow.h
@@ -3,12 +3,12 @@
 
 //======================================================================
 //
-//	red_ow, 192x32@4, 
+//	red_ow, 256x32@4, 
 //	+ palette 15 entries, not compressed
-//	+ 96 tiles Metatiled by 2x4 not compressed
-//	Total size: 30 + 3072 = 3102
+//	+ 128 tiles Metatiled by 2x4 not compressed
+//	Total size: 30 + 4096 = 4126
 //
-//	Time-stamp: 2024-11-24, 23:49:52
+//	Time-stamp: 2024-11-30, 13:50:56
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -17,8 +17,8 @@
 #ifndef GRIT_RED_OW_H
 #define GRIT_RED_OW_H
 
-#define red_owTilesLen 3072
-extern const unsigned short red_owTiles[1536];
+#define red_owTilesLen 4096
+extern const unsigned short red_owTiles[2048];
 
 #define red_owPalLen 30
 extern const unsigned short red_owPal[16];
--- source/main.c
+++ source/main.c
@@ -9,52 +9,82 @@ void obj_test(OBJ_ATTR * obj);
 
 OBJ_ATTR obj_buffer[128];
 
+typedef enum {
+    DOWN = 0, UP = 4, LEFT = 8, RIGHT = 12 
+} WALK_N;
+
+typedef struct overworld {
+    OBJ_ATTR * obj;
+    int animf;
+    int firstanimframe;
+    int currentframe;
+    WALK_N walkcycleN;
+} overworld;
+
 OBJ_ATTR *red = &obj_buffer[0];
 
-void handleredanim(void);
+overworld PLAYER = {
+    &obj_buffer[0],
+    0,
+    0,
+    1,
+    DOWN
+};
+
+void handle_PLAYER_anim(void);
+void handle_input(void);
 
 int fcount = 0;
 
 int main()
 {
-    int x = 0, y = 0;
+    int x = 112, y = 64;
 
-    obj_set_pos(red, x, y);
     irq_init(NULL);
     irq_add(II_VBLANK, NULL);
     
     REG_DISPCNT= DCNT_OBJ | DCNT_OBJ_1D;   
-
+    
     memcpy(&tile_mem[4][0], red_owTiles, red_owTilesLen);
     memcpy(pal_obj_mem, red_owPal, red_owPalLen);
 
     oam_init(obj_buffer, 128);
 
     obj_test(red);
+    obj_set_pos(red, x, y);
     while (1)
     {
-        fcount++;
+//      fcount++;
         VBlankIntrWait();
-        handleredanim();
+        key_poll();
+//      obj_test(red);       
+        handle_PLAYER_anim();
         oam_copy(oam_mem, obj_buffer, 1);
     }
     return 0;
 }
 
-void handleredanim(void)
+void handle_PLAYER_anim(void)
 {
-    static int ind = 2;
-    static int inc = 0;
-    if (!(fcount%12))
+    if (key_held(KEY_DOWN))
+        PLAYER.firstanimframe = 0;
+    else if (key_held(KEY_UP))
+        PLAYER.firstanimframe = 4;
+    else if (key_held(KEY_LEFT))
+        PLAYER.firstanimframe = 8;
+    else if (key_held(KEY_RIGHT))
+        PLAYER.firstanimframe = 12;
+    else
     {
-        red->attr2 = (red->attr2 & 0xfc00) | 8*ind;
-        if (inc && ind == 2) inc = 0;
-        if (!inc && ind == 0) inc = 1;
-        if (inc) ind++;
-        else ind--;
+        PLAYER.obj->attr2 = (PLAYER.obj->attr2 & 0xfc00) | 8*((PLAYER.currentframe=1) + PLAYER.firstanimframe);
+        return;
+    }
+    if (!(PLAYER.animf++%12))
+    {
+        PLAYER.obj->attr2 = (PLAYER.obj->attr2 & 0xfc00) | 8*(PLAYER.currentframe++%4 + PLAYER.firstanimframe);
     }
 }
-
+ 
 void obj_test(OBJ_ATTR * obj)
 {
     u32 tid = 8, pb = 0; //tileid and palbank
@@ -63,7 +93,7 @@ void obj_test(OBJ_ATTR * obj)
     ATTR1_SIZE_32,
     ATTR2_PALBANK(pb) | tid); 
 
-    oam_copy(oam_mem, obj_buffer, 1);
+//  oam_copy(oam_mem, obj_buffer, 1);
 }
   
   
